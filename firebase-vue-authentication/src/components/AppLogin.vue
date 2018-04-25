f3ac2329e7e1f320d223a9f17949c92bb1198fdd
progayk <drum.ayk@gmail.com>
2018-04-26 01:01:33 +0300

    create login page and style it with bulma

--- firebase-vue-authentication/src/components/AppLogin.vue
+++ firebase-vue-authentication/src/components/AppLogin.vue
@@ -1,12 +1,37 @@
 <template>
-  <div class="login">
-    <h3>Sign In</h3>
-    <!--<label for="email"></label>-->
-    <input type="email" name="email" placeholder="Email"><br>
-    <input type="password" placeholder="Password"><br>
-    <button>Connection</button>
-    <br>
-    <p>You don't have an account? You can create one.</p>
+  <div>
+    <section class="section">
+      <div class="container is-fluid columns is-centered">
+        <div class="column is-one-quarter">
+          <h1 class="title">Login</h1>
+          <div class="field">
+            <div class="control has-icons-left has-icons-right">
+              <input class="input is-success" type="text" placeholder="username">
+              <span class="icon is-small is-left">
+      <i class="fas fa-user"></i>
+    </span>
+              <span class="icon is-small is-right">
+      <i class="fas fa-check"></i>
+    </span>
+            </div>
+            <p class="help is-success">This username is available</p>
+          </div>
+
+          <div class="field">
+            <div class="control has-icons-left has-icons-right">
+              <input class="input is-danger" type="email" placeholder="your_mail@">
+              <span class="icon is-small is-left">
+      <i class="fas fa-envelope"></i>
+    </span>
+              <span class="icon is-small is-right">
+      <i class="fas fa-exclamation-triangle"></i>
+    </span>
+            </div>
+            <p class="help is-danger">This email is invalid</p>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -20,6 +45,6 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss">
+  @import '~bulma/bulma'
 </style>
