c6c3f1428baf32195e566b22ffa141dfe7b0eed0
progayk <drum.ayk@gmail.com>
2018-04-25 21:59:37 +0300

    init Vue2 + Firebase authentication system project

---
+++ firebase-vue-authentication/src/components/AppLogin.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="login">
+    <h3>Sign In</h3>
+    <!--<label for="email"></label>-->
+    <input type="email" name="email" placeholder="Email"><br>
+    <input type="password" placeholder="Password"><br>
+    <button>Connection</button>
+    <br>
+    <p>You don't have an account? You can create one.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppLogin',
+  data () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
