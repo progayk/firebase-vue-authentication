5eeb5b701ebc2fd085c7e8ed1dae79b686a7c35f
progayk <drum.ayk@gmail.com>
2018-04-26 23:46:05 +0300

    add redirect bad urls to /login path

--- firebase-vue-authentication/src/router/index.js
+++ firebase-vue-authentication/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import 'semantic-ui-css/semantic.min.css'
 import HelloWorld from '@/components/HelloWorld'
 import AppLogin from '@/components/AppLogin'
 import AppSignup from '@/components/AppSignup'
@@ -10,14 +9,18 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/hello',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '*',
+      redirect: '/login'
     },
     {
       path: '/login',
       name: 'AppLogin',
       component: AppLogin
+    },
+    {
+      path: '/hello',
+      name: 'HelloWorld',
+      component: HelloWorld
     },
     {
       path: '/signup',
