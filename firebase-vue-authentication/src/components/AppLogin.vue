091675b36052471bd1543ac9903fac54421bdb44
progayk <drum.ayk@gmail.com>
2018-04-26 22:39:58 +0300

    add firebase auth system in AppSignup component

--- firebase-vue-authentication/src/components/AppLogin.vue
+++ firebase-vue-authentication/src/components/AppLogin.vue
@@ -4,22 +4,21 @@
       <div class="container is-fluid columns is-centered">
         <div class="column is-one-quarter">
           <!--<h1 class="title">Login</h1>-->
+          <!--<div class="field">-->
+            <!--<div class="control has-icons-left has-icons-right">-->
+              <!--<input class="input is-success is-rounded" type="text" placeholder="username" autofocus>-->
+              <!--<span class="icon is-small is-left">-->
+      <!--<i class="fas fa-user"></i>-->
+    <!--</span>-->
+              <!--<span class="icon is-small is-right">-->
+      <!--<i class="fas fa-check"></i>-->
+    <!--</span>-->
+            <!--</div>-->
+            <!--&lt;!&ndash;<p class="help is-success">This username is available</p>&ndash;&gt;-->
+          <!--</div>-->
           <div class="field">
             <div class="control has-icons-left has-icons-right">
-              <input class="input is-success is-rounded" type="text" placeholder="username" autofocus>
-              <span class="icon is-small is-left">
-      <i class="fas fa-user"></i>
-    </span>
-              <span class="icon is-small is-right">
-      <i class="fas fa-check"></i>
-    </span>
-            </div>
-            <!--<p class="help is-success">This username is available</p>-->
-          </div>
-
-          <div class="field">
-            <div class="control has-icons-left has-icons-right">
-              <input class="input is-danger is-rounded" type="email" placeholder="your_mail@">
+              <input class="input is-danger is-rounded" type="email" v-model="email" placeholder="your_mail@">
               <span class="icon is-small is-left">
       <i class="fas fa-envelope"></i>
     </span>
@@ -29,9 +28,21 @@
             </div>
             <!--<p class="help is-danger">This email is invalid</p>-->
           </div>
-          <div class="buttons is-centered">
-            <a class="button is-outlined is-primary is-rounded" v-on:click="login">Login</a>
-            <router-link to="/signup"><a class="button is-outlined is-info is-rounded">Signup</a></router-link>
+
+          <div class="field">
+            <div class="control has-icons-left has-icons-right">
+              <input class="input is-info is-rounded" type="password" placeholder="password" autofocus>
+              <span class="icon is-small is-left">
+      <i class="fas fa-lock"></i>
+    </span>
+              <span class="icon is-small is-right">
+      <i class="fas fa-check"></i>
+    </span>
+            </div>
+          </div>
+          <div class="buttons is-left">
+            <a class="button is-primary is-rounded" v-on:click="login">Login</a>
+            <router-link to="/signup"><a class="button is-outlined is-info is-rounded">Sign up</a></router-link>
           </div>
         </div>
       </div>
--- firebase-vue-authentication/src/components/AppSignup.vue
+++ firebase-vue-authentication/src/components/AppSignup.vue
@@ -4,22 +4,35 @@
       <div class="container is-fluid columns is-centered">
         <div class="column is-one-quarter">
           <!--<h3 class="title">Sign up</h3>-->
+          <!--<div class="field">-->
+            <!--<div class="control has-icons-left has-icons-right">-->
+              <!--<input class="input is-success is-rounded" type="text" placeholder="username" autofocus>-->
+              <!--<span class="icon is-small is-left">-->
+      <!--<i class="fas fa-user"></i>-->
+    <!--</span>-->
+              <!--<span class="icon is-small is-right">-->
+      <!--<i class="fas fa-check"></i>-->
+    <!--</span>-->
+            <!--</div>-->
+            <!--<p class="help is-success">This username is available</p>-->
+          <!--</div>-->
+
           <div class="field">
             <div class="control has-icons-left has-icons-right">
-              <input class="input is-success is-rounded" type="text" placeholder="username" autofocus>
+              <input class="input is-danger is-rounded" type="email" v-model="email" placeholder="your_mail@">
               <span class="icon is-small is-left">
-      <i class="fas fa-user"></i>
+      <i class="fas fa-envelope"></i>
     </span>
               <span class="icon is-small is-right">
-      <i class="fas fa-check"></i>
+      <i class="fas fa-exclamation-triangle"></i>
     </span>
             </div>
-            <!--<p class="help is-success">This username is available</p>-->
+            <!--<p class="help is-danger">This email is invalid</p>-->
           </div>
 
           <div class="field">
             <div class="control has-icons-left has-icons-right">
-              <input class="input is-info is-rounded" type="password" placeholder="password" autofocus>
+              <input class="input is-info is-rounded" type="password" v-model="password" placeholder="password">
               <span class="icon is-small is-left">
       <i class="fas fa-lock"></i>
     </span>
@@ -29,24 +42,12 @@
             </div>
           </div>
 
-          <div class="field">
-            <div class="control has-icons-left has-icons-right">
-              <input class="input is-danger is-rounded" type="email" placeholder="your_mail@">
-              <span class="icon is-small is-left">
-      <i class="fas fa-envelope"></i>
-    </span>
-              <span class="icon is-small is-right">
-      <i class="fas fa-exclamation-triangle"></i>
-    </span>
-            </div>
-            <!--<p class="help is-danger">This email is invalid</p>-->
-            <label class="checkbox">
-              <input type="checkbox">
-              I agree to the <a href="#">terms and conditions</a>
-            </label>
-          </div>
-          <div class="buttons is-centered">
-            <a class="button is-outlined is-primary is-rounded">Submit</a>
+          <label class="checkbox">
+            <input type="checkbox">
+            I agree to the <a href="#">terms and conditions</a>
+          </label>
+          <div class="buttons is-left">
+            <a class="button is-primary is-rounded" v-on:click="signUp">Submit</a>
             <router-link to="/login"><a class="button is-outlined is-info is-rounded">Login</a></router-link>
           </div>
         </div>
@@ -56,18 +57,34 @@
 </template>
 
 <script>
+import firebase from 'firebase'
 export default {
   name: 'AppSignup',
   data () {
-    return {}
+    return {
+      email: '',
+      password: ''
+    }
   },
-  methods: {}
+  methods: {
+    signUp: function () {
+      firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(
+        function (user) {
+          alert('Your account has been created!')
+        },
+        function (err) {
+          alert('Oops, ' + err.message)
+        }
+      )
+    }
+  }
 }
 </script>
 
 <style lang="scss">
   @import '~bulma/bulma';
   label {
-    margin-top: 20px;
+    margin-top: 10px;
+    margin-bottom: 16px;
   }
 </style>
